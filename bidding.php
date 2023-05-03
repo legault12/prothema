3eaa6e7381aa23c00ed02ae5c6ca70eda561a8eb
jeremyfrancis55 <80563976+jeremyfrancis55@users.noreply.github.com>
2023-05-02 20:39:32 -0700

    update bidding page

--- bidding.php
+++ bidding.php
@@ -46,12 +46,6 @@
       <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
       <nav id="navbar" class="navbar">
-        <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-          <li><a class="getstarted scrollto" href="#about">My Account</a></li>
-        </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
 
