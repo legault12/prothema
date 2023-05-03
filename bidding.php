2e3905dd3e5c7b80830f78b0663338ead0129626
jeremyfrancis55 <80563976+jeremyfrancis55@users.noreply.github.com>
2023-05-02 20:43:48 -0700

    Update bidding.php

--- bidding.php
+++ bidding.php
@@ -46,7 +46,7 @@
       <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
       <nav id="navbar" class="navbar">
-        <ul></ul>
+        <ul><li></li></ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
 
