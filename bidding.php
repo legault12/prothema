47e381c7e4083ec6905dd2d2d5baf4f73b41e9ff
jeremyfrancis55 <80563976+jeremyfrancis55@users.noreply.github.com>
2023-05-02 20:58:48 -0700

    Update bidding.php

--- bidding.php
+++ bidding.php
@@ -53,5 +53,30 @@
       </nav><!-- .navbar -->
     </div>
   </header><!-- End Header -->
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 pt-2 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+          <h1>Your home for fleet procurement and bidding</h1>
+          <ul>
+            <li><i class="ri-check-line"></i> Dolorem ratione dolorum</li>
+            <li><i class="ri-check-line"></i> Quo nihil natus ea non pariatur optio occaecati</li>
+            <li><i class="ri-check-line"></i> Duis aute irure dolor in reprehenderit in</li>
+          </ul>
+          <div class="mt-3">
+            <a href="bidding.php" class="btn-get-started scrollto">Start a Bid</a>
+            <a href="" class="btn-get-quote">Join Our Service</a>
+          </div>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img">
+          <img src="assets/img/istockphoto-1210329336-612x612.jpg" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
 </body>
 </html>
