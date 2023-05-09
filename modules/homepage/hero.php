4260377640eb82e207fa0cc681e3a3833331aff9
legault12 <131191635+legault12@users.noreply.github.com>
2023-05-08 20:03:46 -0700

    separated out hero.php

---
+++ modules/homepage/hero.php
@@ -0,0 +1,23 @@
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 pt-2 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+          <h1>Your source for competitive fleet procurement</h1>
+          <ul>
+            <li><i class="ri-check-line"></i> Public Sector Focused</li>
+            <li><i class="ri-check-line"></i> Get Dealers to compete for your business</li>
+          </ul>
+          <div class="mt-3">
+            <a href="bidding.php" class="btn-get-started scrollto">Start a Bid</a>
+            <a href="" class="btn-get-quote">Join Our Service</a>
+          </div>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img">
+          <img src="assets/img/istockphoto-1210329336-612x612.jpg" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
