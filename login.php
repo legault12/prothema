d1e5b7e225cf1727602397df7630072aef79f7be
legault12 <131191635+legault12@users.noreply.github.com>
2023-05-08 21:52:04 -0700

    Update login.php

--- login.php
+++ login.php
@@ -1,66 +1,104 @@
 <?php
-  $path = $_SERVER['DOCUMENT_ROOT'];
-  $path .= "modules/universal/db_connection.php";
-  include_once($path);
-  ?>
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "modules/universal/db_connection.php";
+include_once($path);
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Prothema - Login</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+    <title>Prothema - Login</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
 
-  <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
-  <link href="assets/css/login.css" rel="stylesheet">
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/login.css" rel="stylesheet">
 
 </head>
 
 <body>
-  <?php include 'modules/universal/header.php'; ?>
+    <?php
+    // Replace the database credentials with your own
+    $host = "localhost";
+    $username = "frontend";
+    $password = ".Zfb_oMsA8EgvFZTYqr9";
+    $database = "prothemadb";
 
-  <main id="main">
-    <div class="login-container">
-      <h2>Login</h2>
-      <form action="user_login/login_process.php" method="post">
-        <input type="text" name="username" placeholder="Username" required>
-        <input type="password" name="password" placeholder="Password" required>
-        <input type="submit" value="Log in">
-      </form>
-    </div>
-  </main>
+    // Create a new database connection
+    $conn = mysqli_connect($host, $username, $password, $database);
 
-  <?php include 'modules/universal/footer.php'; ?>
+    // Check if the connection was successful
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
+    // Prepare a SQL query to select all rows from the users table
+    $sql = "SELECT * FROM users";
 
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+    // Execute the query and get the result set
+    $result = mysqli_query($conn, $sql);
+
+    // Check if any rows were returned
+    if (mysqli_num_rows($result) > 0) {
+        // Loop through each row and display the values
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "ID: " . $row["id"] . " - Name: " . $row["name"] . " - Email: " . $row["email"] . "<br>";
+        }
+    } else {
+        echo "No results found.";
+    }
+
+    // Close the database connection
+    mysqli_close($conn);
+    ?>
+
+    <?php include 'modules/universal/header.php'; ?>
+
+    <main id="main">
+        <div class="login-container">
+            <h2>Login</h2>
+            <form action="user_login/login_process.php" method="post">
+                <input type="text" name="username" placeholder="Username" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <input type="submit" value="Log in">
+            </form>
+        </div>
+    </main>
+
+    <?php include 'modules/universal/footer.php'; ?>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
 
 </body>
 
