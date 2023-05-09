c121d0ca9c5a15cb5715026861da52c6423f8cd3
legault12 <131191635+legault12@users.noreply.github.com>
2023-05-08 21:22:51 -0700

    Update login.php

--- login.php
+++ login.php
@@ -36,7 +36,7 @@
   <main id="main">
     <div class="login-container">
       <h2>Login</h2>
-      <form action="login_process.php" method="post">
+      <form action="user_login/login_process.php" method="post">
         <input type="text" name="username" placeholder="Username" required>
         <input type="password" name="password" placeholder="Password" required>
         <input type="submit" value="Log in">
