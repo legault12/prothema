4d2849eae471ad4d6836f9beb3c3c9e4520eda4d
jeremyfrancis55 <80563976+jeremyfrancis55@users.noreply.github.com>
2023-05-08 20:33:11 -0700

    Update header.php

--- modules/universal/header.php
+++ modules/universal/header.php
@@ -4,7 +4,7 @@
 <header id="header" class="fixed-top ">
   <div class="container d-flex align-items-center justify-content-between">
 
-    <h1 class="logo"><a href="index.html"><?= $logo ?></a></h1>
+    <h1 class="logo"><a href="index.php"><?= $logo ?></a></h1>
     <!-- Uncomment below if you prefer to use an image logo -->
     <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
@@ -13,7 +13,7 @@
         <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
         <li><a class="nav-link scrollto" href="#about">About</a></li>
         <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-        <li><a class="getstarted scrollto" href="#about">My Account</a></li>
+        <li><a class="getstarted scrollto" href="#">My Account</a></li>
       </ul>
       <i class="bi bi-list mobile-nav-toggle"></i>
     </nav><!-- .navbar -->
