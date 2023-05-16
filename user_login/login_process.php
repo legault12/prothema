06d49d7031d8092049beff59a22b9476362e986e
legault12 <131191635+legault12@users.noreply.github.com>
2023-05-15 21:35:09 -0700

    testing with log file and temporarily removing password hash for testing

--- user_login/login_process.php
+++ user_login/login_process.php
@@ -5,6 +5,9 @@ session_start();
 // include the database connection file
 include_once '../modules/universal/db_connection.php';
 
+// open a log file
+$logFile = fopen("log.txt", "w");
+
 // prepare and execute the SQL statement
 $stmt = $conn->prepare("SELECT * FROM users WHERE username = :username");
 $stmt->bindParam(':username', $_POST['username']);
@@ -14,16 +17,25 @@ $stmt->execute();
 if($stmt->rowCount() > 0) {
     // get the user
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    fwrite($logFile, "User found in database.\n");
 
     // verify the password
-    if(password_verify($_POST['password'], $user['password'])) {
+    if ($_POST['password'] === $user['password']) {
         // store the user's ID in the session
         $_SESSION['user_id'] = $user['id'];
+        fwrite($logFile, "Password verified.\n");
         
         // redirect to the account page
+        fclose($logFile);
         header("Location: account.php");
         exit;
+    } else {
+        fwrite($logFile, "Password verification failed.\n");
+        fclose($logFile);
     }
+} else {
+    fwrite($logFile, "User not found in database.\n");
+    fclose($logFile);
 }
 
 // if the script hasn't exited by now, the login attempt was unsuccessful
